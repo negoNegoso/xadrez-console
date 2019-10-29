90888035489700ab9757848b9e6f52312d6ec1f1
yvillanova <yurimoreiravillanova1@gmail.com>
2019-10-29 18:58:34 -0300

    Tabuleiro iniciado

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,10 +12,8 @@ namespace xadrez_console
         static void Main(string[] args)
         {
 
-            Posicao p;
-
-            p = new Posicao(3, 4);
-            Console.WriteLine("Posição: " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
 
             Console.ReadKey();
 
---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, int qteMovimentos, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.qteMovimentos = 0;
+            this.tabuleiro = tabuleiro;
+        }
+    }
+}
