6544c2bcb6c787c3a0224a8defd569516ce55a1b
yvillanova <yurimoreiravillanova1@gmail.com>
2019-10-30 18:40:47 -0300

    ulti

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -5,20 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using xadrez_console.tabuleiro;
 using xadrez;
+
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                /*
+                 Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
 
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
+                Tela.imprimirTabuleiro(tab);
+                 */
 
-            Tela.imprimirTabuleiro(tab);
+                PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+
+                Console.WriteLine(pos);
+                Console.WriteLine(pos.toPosicao());
+            }
+
+
+            catch (TabuleiroException e)
+            {
+                
+                Console.WriteLine(e.Message);
+            }
+
 
             Console.ReadKey();
         }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using xadrez_console.tabuleiro;
 
 namespace xadrez_console.tabuleiro
 {
@@ -16,7 +17,7 @@ namespace xadrez_console.tabuleiro
         {
             this.linhas = linhas;
             this.colunas = colunas;
-            this.pecas = new Peca[linhas,colunas];
+            this.pecas = new Peca[linhas, colunas];
         }
 
         public Peca peca(int linha, int coluna)
@@ -24,10 +25,45 @@ namespace xadrez_console.tabuleiro
             return pecas[linha, coluna];
         }
 
-        public void colocarPeca(Peca p, Posicao pos) {
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida");
+            }
+        }
+
+
 
 
     }
